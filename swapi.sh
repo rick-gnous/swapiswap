2581c8e0c3b18f183182a5a0f139d377aff8dbe1
Rick <rick@gnous.eu>
2020-06-18 09:44:22 +0200

    Activation du swap sans reboot, close #2

--- swapi.sh
+++ swapi.sh
@@ -21,10 +21,16 @@ then
     exit 1
 fi
 
+echo "Creation du swap"
 dd if=/dev/zero of=/swapfile count=${sizeSwap}M
 mkswap /swapfile
+
+echo "Mise à jour du fstab"
 echo >> /etc/fstab
-echo "# Swap" >> /etc/fstab
+echo "# Swap genere avec swapi !" >> /etc/fstab
 echo "/swapfile	none	swap	sw,loop	0 0" >> /etc/fstab
+
+echo "Montage du swap"
+swapon /swapfile
 
 echo "Vérifiez que le swap est bien activé grace à la commande : sudo swapon --show"
